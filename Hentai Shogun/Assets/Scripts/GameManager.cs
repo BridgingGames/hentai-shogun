6bdecef0667e150b472bb68c6188772d22c04973
Fiery Fox <jiaf.email@gmail.com>
2019-11-06 21:20:22 -0300

    UI Switch
    Creation of method that switch between different Canvas (UI groups).

---
+++ Hentai Shogun/Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    // SECTION: VARIABLES
+    [Header("UI Variables")]
+    [SerializeField] private Canvas[] canvasList;
+    [SerializeField] private int currentCanvas;
+    [SerializeField] private CanvasGroup fadeCanvasGroup;
+    [SerializeField] private float fadeDuration;
+
+    // Use this for initialization
+    void Start()
+    {
+        SwitchCanvas(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // SECTION: UI
+    public void SwitchCanvas(int canvas)
+    {
+        fadeCanvasGroup.blocksRaycasts = true;
+        
+        StartCoroutine(CanvasFadeIn(canvas));
+    }
+
+    public IEnumerator CanvasFadeIn(int canvas)
+    {
+        float fadeTimer = 0.0f;
+
+        while (fadeCanvasGroup.alpha < 1.0f)
+        {
+            fadeCanvasGroup.alpha = Mathf.Lerp(0.0f, 1.0f, (fadeTimer / fadeDuration));
+            fadeTimer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        canvasList[currentCanvas].enabled = false;
+        canvasList[canvas].enabled = true;
+        currentCanvas = canvas;
+
+        StartCoroutine(CanvasFadeOut(canvas)); 
+    }
+
+    public IEnumerator CanvasFadeOut(int canvas)
+    {
+        float fadeTimer = 0.0f;
+
+        while (fadeCanvasGroup.alpha > 0.0f)
+        {
+            fadeCanvasGroup.alpha = Mathf.Lerp(1.0f, 0.0f, (fadeTimer / fadeDuration));
+            fadeTimer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        fadeCanvasGroup.blocksRaycasts = false;
+    }
+}
